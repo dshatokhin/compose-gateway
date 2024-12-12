767652e283fc8360da90faefceae06205fda8913
Denis Shatokhin <d_shatokhin@outlook.com>
2024-12-13 00:28:06 +0200

    some improvements

--- README.md
+++ README.md
@@ -1,1 +1,64 @@
 # Compose Gateway
+
+Quick and simple implementation of experimental
+[Standalone Deployment Mode](https://gateway.envoyproxy.io/docs/tasks/operations/standalone-deployment-mode/)
+for Docker Compose.
+
+Containers run with `host_network` and `root` user, be careful!
+
+Also requires full access to docker socket!
+
+## Quick Start
+
+```shell
+# start some workload
+$ docker compose --file ./compose-worloads.yaml up --detach
+[+] Running 2/2
+ ✔ Container compose-gateway-apache-1  Started                        0.2s
+ ✔ Container compose-gateway-nginx-1   Started                        0.2s
+
+# start compose-gateway
+$ docker compose up --detach
+[+] Running 3/3
+ ✔ Container compose-gateway-controller-1     Started                        0.2s
+ ✔ Container compose-gateway-certgen-1        Exited                         1.2s
+ ✔ Container compose-gateway-envoy-gateway-1  Started                        1.3s
+
+# test the workloads
+$ curl --head localhost:80 -H "Host: nginx.example.org"
+HTTP/1.1 200 OK
+server: nginx/1.27.3
+date: Sun, 04 May 2025 17:12:10 GMT
+content-type: text/html
+content-length: 615
+last-modified: Tue, 26 Nov 2024 15:55:00 GMT
+etag: "6745ef54-267"
+accept-ranges: bytes
+
+$ curl --head localhost:80 -H "Host: apache.example.org"
+HTTP/1.1 200 OK
+date: Sun, 04 May 2025 17:12:17 GMT
+server: Apache/2.4.62 (Unix)
+last-modified: Mon, 11 Jun 2007 18:53:14 GMT
+etag: "2d-432a5e4a73a80"
+accept-ranges: bytes
+content-length: 45
+content-type: text/html
+```
+
+## Cleanup
+
+```shell
+$ docker compose down
+[+] Running 4/4
+ ✔ Container compose-gateway-envoy-gateway-1  Removed                        0.1s
+ ✔ Container compose-gateway-certgen-1        Removed                        0.0s
+ ✔ Container compose-gateway-controller-1     Removed                        10.1s
+ ! Network compose-gateway_default            Resource is still in use       0.0s
+
+$ docker compose --file ./compose-worloads.yaml down
+[+] Running 3/3
+ ✔ Container compose-gateway-nginx-1   Removed                        0.1s
+ ✔ Container compose-gateway-apache-1  Removed                        1.1s
+ ✔ Network compose-gateway_default     Removed                        0.1s
+```
--- controller/main.sh
+++ controller/main.sh
@@ -1,125 +1,60 @@
 #!/usr/bin/env bash
 
-cdsConfigPath=${CDS_CONFIG_PATH:-"/config/cds.yaml"}
-vhdsConfigPath=${VHDS_CONFIG_PATH:-"/config/vhds.yaml"}
-statePath=${STATE_PATH:-"/config/state.yaml"}
-updateInterval=${CONFIG_UPDATE_INTERVAL:-"10"}
-tempDir=$(mktemp --directory)
+outputFile="resources.yaml"
+pklFile="main.pkl"
+tempDir=$(mktemp -d)
+configDir=${COMPOSE_GATEWAY_CONFIG_DIR:-"/config"}
+interval=${COMPOSE_GATEWAY_UPDATE_INTERVAL:-"5"}
 
-initConfig() {
-  mkdir -p "/config"
-  cp /controller/bootstrap.yaml /config/bootstrap.yaml
-  yq --null-input --output-format yaml '{"resources":[]}' >"$cdsConfigPath"
-  yq --null-input --output-format yaml '{"resources":[]}' >"$vhdsConfigPath"
-  touch /var/run/controller
+# creates an empty output file
+touch "${tempDir}/${outputFile}"
+
+# creates default config file
+pkl eval "$pklFile" -p init=true -m "$configDir"
+
+info() {
+  echo "INFO: $*"
 }
 
-getContainer() {
-  local name="${1}"
+error() {
+  echo "ERROR: $*"
+} >&2
 
-  docker inspect "$name" --type container --format json | yq --indent 0 -M
+getAllContainers() {
+  local ids containers
+
+  ids=$(docker ps -q)
+  containers=$(docker inspect $ids)
+
+  if echo "$containers" | yq --exit-status >/dev/null; then
+    echo "$containers"
+  else
+    echo "[]"
+    error "Failed to parse containers JSON object"
+  fi
 }
 
-getAnnotations() {
-  local container="${1}"
+generateGatewayResources() {
+  local payload=${1}
 
-  yq --output-format json --indent 0 -M -r '.[].HostConfig.Annotations // ""' <<<"$container"
+  pkl eval "$pklFile" -p payload="$payload" -p file="$outputFile" -m "$tempDir" >/dev/null
+  if ! diff -q "${tempDir}/${outputFile}" "${configDir}/${outputFile}" &>/dev/null; then
+    info "Changes in config detected, applying now..."
+    cp "${tempDir}/${outputFile}" "${configDir}/${outputFile}"
+  fi
 }
 
-getName() {
-  local container="${1}"
+exit=0
+trap exiting SIGINT
 
-  yq --indent 0 -M -r '.[].Name | sub("/"; "") // ""' <<<"$container"
+exiting() {
+  echo ""
+  echo "Exiting main loop"
+  exit=1
 }
 
-getIpAddresses() {
-  local container="${1}"
-
-  yq --output-format json --indent 0 -M '[.[].NetworkSettings.Networks[].IPAddress]' <<<"$container"
-}
-
-updateConfig() {
-  local annotations="${1}"
-  local name="${2}"
-  local configFile="${3}"
-  local type="${4}"
-  local action="${5}"
-  local ipAddresses="${6}"
-
-  pkl eval /controller/main.pkl \
-    -p annotations="$annotations" \
-    -p container="$name" \
-    -p configFile="$configFile" \
-    -p type="$type" \
-    -p action="$action" \
-    -p ipAddresses="$ipAddresses"
-}
-
-updateState() {
-  local name="${1}"
-  local annotations="${2}"
-
-  yq ".${name}.annotations = ${annotations}" "$statePath"
-}
-
-triggerReload() {
-  touch "/config/updated" && mv "/config/updated" "/config/trigger" && rm "/config/trigger"
-}
-
-main() {
-  local containers
-
-  containers=$(docker ps -q)
-  yq --null-input --output-format yaml '{"resources":[]}' >"${cdsConfigPath}.temp"
-  yq --null-input --output-format yaml '{"resources":[]}' >"${vhdsConfigPath}.temp"
-
-  for c in $containers; do
-    local annotations name containerJson
-    local compose_gateway_org_enabled=""
-
-    containerJson=$(getContainer "$c")
-    annotations=$(getAnnotations "$containerJson")
-    addresses=$(getIpAddresses "$containerJson")
-
-    compose_gateway_org_enabled=$(yq '."compose.gateway.org/enabled"' --input-format json <<<"$annotations")
-
-    if [[ "$compose_gateway_org_enabled" == "true" ]]; then
-      name=$(getName "$containerJson")
-      # echo "Exposing container $name"
-
-      updateConfig "$annotations" "$name" "${cdsConfigPath}.temp" "cds" "add" "$addresses" >"${cdsConfigPath}.updated"
-      cp "${cdsConfigPath}.updated" "${cdsConfigPath}.temp"
-
-      updateConfig "$annotations" "$name" "${vhdsConfigPath}.temp" "vhds" "add" >"${vhdsConfigPath}.updated"
-      cp "${vhdsConfigPath}.updated" "${vhdsConfigPath}.temp"
-    fi
-  done
-
-  diff -q "${cdsConfigPath}.temp" "$cdsConfigPath" || mv "${cdsConfigPath}.temp" "$cdsConfigPath"
-  diff -q "${vhdsConfigPath}.temp" "$vhdsConfigPath" || mv "${vhdsConfigPath}.temp" "$vhdsConfigPath"
-}
-
-echo "Initialising..."
-initConfig
-
-echo "Starting main loop..."
-while true; do
-  main
-  sleep "$updateInterval"
+info "Starting main loop"
+while [ $exit -eq 0 ]; do
+  generateGatewayResources "$(getAllContainers)"
+  sleep "$interval"
 done
-
-# [[ -n $annotations && -n $name && (-n $currentCdsConfig || -n $currentVhdsConfig) ]] && config=$(generate "$annotations" "$name")
-# [[ -n $config ]] && echo "CLUSTERS:"
-
-# patchCds=$(yq '.clusters' --output-format json --indent 0 --no-colors <<<"$config")
-# patchRds=$(yq '.routes' --output-format json --indent 0 --no-colors <<<"$config")
-# patchVhds=$(yq '.virtual_hosts' --output-format json --indent 0 --no-colors <<<"$config")
-# echo $config | yq -pj -oy
-# echo $currentVhds
-# yq ". += $patchCds" "$cds_config_path"
-# yq --inplace ". + $patchCds" "$cds_config_path"
-# yq --inplace ". + $patchVhds" "$vhds_config_path"
-# yq ". += $patchCds" "$cds_config_path"
-# yq -n "$currentRds + $patchRds"
-# yq '.clusters' --input-format json --output-format json -I0 --no-colors <<<"$config"
-# yq '.routes' --input-format json --output-format json -I0 --no-colors <<<"$config"
